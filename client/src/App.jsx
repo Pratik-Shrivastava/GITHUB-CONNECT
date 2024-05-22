1fe314b4734a2bbe9667c042edeaf56c2f512e5d
sneha-2510 <ranisnehaaa@gmail.com>
2024-05-23 01:52:45 +0530

    feat: Routes & Basis Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import Logo from "./components/Logo.jsx"
+import Navbar from "./components/Navbar.jsx"
+
 function App() {
 
   return (
     <>
-      <h1>Getting started with vite +react.</h1>
+
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  Route, RouterProvider, createBrowserRouter, createRoutesFromElements
+} from "react-router-dom"
+// import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Layout from './Layout.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      {/* <Route path='contact' element={<Contact />} /> */}
+      {/* <Route path='user/:userid' element={<User />} /> */}
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
